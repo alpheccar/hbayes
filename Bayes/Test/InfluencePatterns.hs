d404d7d37485463153a19717e318b6e6ee11e105
Sidharth Kapur <sidharthkapur1@gmail.com>
2016-05-19 18:32:37 -0500

    Replace tabs with spaces

--- Bayes/Test/InfluencePatterns.hs
+++ Bayes/Test/InfluencePatterns.hs
@@ -9,16 +9,16 @@ import Test.HUnit.Base(assertBool)
 import Data.Maybe(fromJust)
 
 import Bayes.Examples.Influence
-import Bayes.InfluenceDiagram 
+import Bayes.InfluenceDiagram
 
-testStudentDecisions = do 
-	let result = solveInfluenceDiagram student
-	    (e,pr,s) = studentDecisionVars
-	    l = map decisionToInstantiation result 
-	assertBool "Student Network" $ l == [[[e =: (0::Int)]],[[s =: (0::Int),pr =: False],[s =: (0::Int),pr =: True]]]
-	let result = solveInfluenceDiagram studentSimple
-	    e = studentSimpleDecisionVar
-	    l = map decisionToInstantiation result 
-	assertBool "Simple Student Network" $ l == [[[ e =: (1 :: Int)]]]
+testStudentDecisions = do
+  let result = solveInfluenceDiagram student
+      (e,pr,s) = studentDecisionVars
+      l = map decisionToInstantiation result
+  assertBool "Student Network" $ l == [[[e =: (0::Int)]],[[s =: (0::Int),pr =: False],[s =: (0::Int),pr =: True]]]
+  let result = solveInfluenceDiagram studentSimple
+      e = studentSimpleDecisionVar
+      l = map decisionToInstantiation result
+  assertBool "Simple Student Network" $ l == [[[ e =: (1 :: Int)]]]
 
 
